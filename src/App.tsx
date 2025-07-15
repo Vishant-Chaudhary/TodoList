375ce251cce6a95336c8207a30a302ab86fcde50
Vishant <vishantkumar755@gmail.com>
2025-07-15 22:27:50 +0530

    Create a working todolist

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import InputFeild from './components/InputFeild'
+import type { Todo } from './components/model'
+
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([ ...todos, { id: Date.now(),todo, isDone:false } ]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
+  
+  return (
+    <>
+    <div className="App">
+      <span className='heading'>Taskify</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((t) => (
+        <li>{t.todo} </li>
+      ) )}
+    </div>
+    </>
+  )
+}
+
+export default App
